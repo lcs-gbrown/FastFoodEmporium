2efa37a9a7ad9cd32e21ab64756f26b5b58f5209
gabi brown <gbrown@lcs.on.ca>
2021-12-10 08:44:49 -0500

    Initial Commit

---
+++ FastFoodEmporium/FastFoodEmporiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastFoodEmporiumApp.swift
+//  FastFoodEmporium
+//
+//  Created by gabi brown on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct FastFoodEmporiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
